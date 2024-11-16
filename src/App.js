2917c3bd22c601f79c41471cbf327fe0a1af2b08
Rizwan Alam <rizwancse2020@gmail.com>
2024-11-16 20:34:01 +0530

    React project initiated with tailwind css

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
+       Hello string calc
+      </div>
+    </div>
+  );
+}
+
+export default App;
